757328d17f70e28f762eea1f2c161a50f0bb0b97
Shreyash-30 <shreyash.30@gmail.com>
2025-07-09 08:57:42 +0530

    dropdown menu added

--- script.js
+++ script.js
@@ -21,3 +21,34 @@ prevBtn.addEventListener('click', () => {
   current = (current - 1 + image.length) % image.length;
   showImage(current);
 });
+
+
+const districtNames = {
+  'Pune': ['Sinhgad', 'Rajgad'],
+  'Satara': ['Pratapgad'],
+  'Raigad':['Raigad']
+}
+
+const district = document.getElementById('district');
+const placeSelect = document.getElementById('place')
+
+for(districts in districtNames){
+  const option = document.createElement('option');
+  option.value = districts;
+  option.textContent = districts;
+  district.appendChild(option);
+}
+
+district.addEventListener("change", function () {
+  const selectedDistrict = this.value;
+ 
+
+  if (districtNames[selectedDistrict]) {
+    districtNames[selectedDistrict].forEach(place => {
+      const option = document.createElement("option");
+      option.value = place;
+      option.textContent = place;
+      placeSelect.appendChild(option);
+    });
+  }
+});
